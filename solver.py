e67397115a9eeb8de9f65dfb2274d4d403147458
Brenton Horne <brentonhorne77@gmail.com>
2021-09-24 23:17:10 +1000

    Modularizing Python script; restructuring print part

--- solver.py
+++ solver.py
@@ -35,78 +35,85 @@ def readData(groupNo, depVarNo):
 
     return group, y
 
-# All approximated via sample estimators
-group, y = readData(0, 5)
-m = int(np.max(group))
-n = np.size(y)
-nvec = np.tile(0, m)
-for i in range(0, m):
-    nvec[i] = int(np.size(y[group == i+1]))
+def main():
+    # All approximated via sample estimators
+    group, y = readData(0, 5)
+    m = int(np.max(group))
+    n = np.size(y)
+    nvec = np.tile(0, m)
+    for i in range(0, m):
+        nvec[i] = int(np.size(y[group == i+1]))
 
-ni = int(np.max(nvec))
-ybarvec = np.array([])
-yarr = np.tile(0.5, (m, ni))
+    ni = int(np.max(nvec))
+    ybarvec = np.array([])
+    yarr = np.tile(0.5, (m, ni))
 
-# yarr rows correspond to different groups
-# columns different observations
-for i in range(0, m):
-    for j in range (0, ni):
-        yarr[i, j] = y[group == i + 1][j]
+    # yarr rows correspond to different groups
+    # columns different observations
+    for i in range(0, m):
+        for j in range (0, ni):
+            yarr[i, j] = y[group == i + 1][j]
 
-# Ybar_i
-ybarvec = np.mean(yarr, axis=1)
-# Initial guess of mu under the null
-muNullInit = np.mean(y)
-# Initial guess of var under the null
-varNullInit = np.var(yarr, axis=1)
-# Initialize variables for our loop below
-muNull = muNullInit
-varNull = varNullInit
+    # Ybar_i
+    ybarvec = np.mean(yarr, axis=1)
+    # Initial guess of mu under the null
+    muNullInit = np.mean(y)
+    # Initial guess of var under the null
+    varNullInit = np.var(yarr, axis=1)
+    # Initialize variables for our loop below
+    muNull = muNullInit
+    varNull = varNullInit
 
-# Get variables for the first iteration
-F, J = funjac(muNull, varNull, nvec, yarr, ybarvec)
-eps = -np.linalg.solve(J, F)
-diff = np.sqrt(sum(eps**2)/(m+1))
-
-# Parameters of the loop
-iteration = 0
-itMax = 1e3
-tol = 1e-13
-param = np.tile(0.0, (m+1, 1))
-
-# Iterate until we get muNull and varNull to required
-# tolerance level or we run out of iterations
-while (tol < diff and iteration < itMax):
-    muNull += eps[0]
-    varNull += eps[1:m+1]
+    # Get variables for the first iteration
     F, J = funjac(muNull, varNull, nvec, yarr, ybarvec)
     eps = -np.linalg.solve(J, F)
-    param[0] = muNull
-    param[1:m+1] = np.reshape(varNull, (m, 1))
-    epsRel = np.reshape(eps, (m+1, 1))/param
-    diff = np.sqrt(np.sum(epsRel**2)/(m+1))
-    iteration += 1
+    diff = np.sqrt(sum(eps**2)/(m+1))
 
-# Hypothesis testing
-# Make an array of ybar values corresponding to each element of y
-ybararr = np.tile(1, np.shape(yarr))
-for i in range(0, 6):
-    ybararr[i, :] = ybarvec[i] * np.ones((1, nvec[i]))
+    # Parameters of the loop
+    iteration = 0
+    itMax = 1e3
+    tol = 1e-13
+    param = np.tile(0.0, (m+1, 1))
 
-# Unrestricted MLE of variance
-varUnrest = (1/nvec * np.sum((yarr - ybararr)**2, axis=1))
+    # Iterate until we get muNull and varNull to required
+    # tolerance level or we run out of iterations
+    while (tol < diff and iteration < itMax):
+        muNull += eps[0]
+        varNull += eps[1:m+1]
+        F, J = funjac(muNull, varNull, nvec, yarr, ybarvec)
+        eps = -np.linalg.solve(J, F)
+        param[0] = muNull
+        param[1:m+1] = np.reshape(varNull, (m, 1))
+        epsRel = np.reshape(eps, (m+1, 1))/param
+        diff = np.sqrt(np.sum(epsRel**2)/(m+1))
+        iteration += 1
 
-# Likelihood ratio
-lam = np.prod(np.power(varUnrest/varNull, nvec/2))
-# Test statistic -2ln(lam)
-stat = -2*np.log(lam)
-# Associated p-value
-pval = 1-chi2.cdf(stat, m-1)
+    # Hypothesis testing
+    # Make an array of ybar values corresponding to each element of y
+    ybararr = np.tile(1, np.shape(yarr))
+    for i in range(0, m):
+        ybararr[i, :] = ybarvec[i] * np.ones((1, nvec[i]))
 
-# Print relevant outputs
-print("Null mean = ", muNull)
-print("Null variance = ", varNull)
-print("Unrestricted mean = ", ybarvec)
-print("Unrestricted variance = ", varUnrest)
-print("Test statistic = ", stat)
-print("P-value = ", pval)
+    # Unrestricted MLE of variance
+    varUnrest = (1/nvec * np.sum((yarr - ybararr)**2, axis=1))
+
+    # Likelihood ratio
+    lam = np.prod(np.power(varUnrest/varNull, nvec/2))
+    # Test statistic -2ln(lam)
+    stat = -2*np.log(lam)
+    # Associated p-value
+    pval = 1-chi2.cdf(stat, m-1)
+
+    # Print relevant outputs
+    print("Null mean = ", muNull)
+    for i in range(0, m):
+        print("For group = ", i +1)
+        print("Null variance = ", varNull[i])
+        print("Unrestricted mean = ", ybarvec[i])
+        print("Unrestricted variance = ", varUnrest[i])
+
+    print("Test statistic = ", stat)
+    print("P-value = ", pval)
+
+if __name__ == "__main__":
+    main()
